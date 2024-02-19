13bee35bafbb1ab2ef6b94c840c7cdd81ff7aa67
albertoferri <falberto99@gmail.com>
2024-02-19 14:58:34 +0100

    creato carosello e aggiunti controlli se scorro oltre l'index max

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ const {createApp} = Vue;
 createApp({
     data(){
         return {
+
+            activeSlideIndex: 0,
+
+
+
             slides: [
                 {
                     image: 'img/01.webp',
@@ -31,5 +36,29 @@ createApp({
                 }
             ],
         }
-    }
+    },
+    methods: {
+        nextSlide() {
+            this.activeSlideIndex++;
+
+            // controllo che l'indice non vada oltre l'ultima immagine
+            if (this.activeSlideIndex >= this.slides.length) {
+                this.activeSlideIndex = 0
+            }
+        },
+
+        prevSlide() {
+            this.activeSlideIndex--;
+
+            // qui controllo che l'indice non vada oltre lo zero 
+            if (this.activeSlideIndex < 0) {
+                this.activeSlideIndex = this.slides.length - 1;
+            }
+        },
+
+        changeSlide(index) {
+            console.log(index)
+            this.activeSlideIndex = index
+        },
+    },
 }).mount('#app');
